1d753235dfc695f951804ce6adf8079d745dcaf4
Kirill Scherba <kirill@scherba.ru>
2022-11-02 22:39:16 +0300

    The s3cp app done

--- cmd/s3cp/main.go
+++ cmd/s3cp/main.go
@@ -8,26 +8,41 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"log/syslog"
 	"os"
+	"strings"
+
+	"github.com/teonet-go/teos3"
 )
 
+// Application constants
 const (
 	appName    = "s3cp"
 	appVersion = "0.0.1"
 )
 
+// S3 access variables
 var (
-	accessKey = os.Getenv("TEOS3_ACCESSKEY")
-	secretKey = os.Getenv("TEOS3_SECRETKEY")
-	endpoint  = os.Getenv("TEOS3_ENDPOINT")
-	bucket    = os.Getenv("TEOS3_BUCKET")
-	secure    = true
+	accessKey, secretKey, endpoint, bucket string
+	secure                                 = true
 )
 
-const usage = "s3cp [flags] source target"
+// Application usage message
+const (
+	about = "Teonet " + appName + " application ver " + appVersion + "\n"
+	usage = "s3cp [OPTION] source target\n" +
+		"use s3:/folder_and_object_name to define S3 in source or target\n"
+)
 
 func main() {
-	fmt.Println("Teonet " + appName + " application ver " + appVersion)
+
+	sysLog, err := syslog.New(syslog.LOG_INFO|syslog.LOG_LOCAL7, appName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+	log.SetOutput(sysLog)
 
 	// Application parameters
 	flag.StringVar(&accessKey, "accesskey", accessKey, "S3 storage Access key")
@@ -36,12 +51,110 @@ func main() {
 	flag.StringVar(&bucket, "bucket", bucket, "S3 storage Bucket")
 	flag.BoolVar(&secure, "secure", secure, "set secure=false to enable insecure (HTTP) access")
 
-	args := flag.Args()
-	if len(args) < 2 {
-		fmt.Println(usage)
-		fmt.Println(args)
+	// Define new flag usage function and parse flag
+	flagUsage := flag.Usage
+	flag.Usage = func() {
+		fmt.Print(about + "\n" + usage + "\n")
+		flagUsage()
+		fmt.Println()
+	}
+	flag.Parse()
+
+	// Set parameters from env
+	if len(accessKey) == 0 {
+		accessKey = os.Getenv("TEOS3_ACCESSKEY")
+	}
+	if len(secretKey) == 0 {
+		secretKey = os.Getenv("TEOS3_SECRETKEY")
+	}
+	if len(endpoint) == 0 {
+		endpoint = os.Getenv("TEOS3_ENDPOINT")
+	}
+	if len(bucket) == 0 {
+		bucket = os.Getenv("TEOS3_BUCKET")
+	}
+
+	// Check parameters
+	if len(accessKey) == 0 || len(secretKey) == 0 || len(endpoint) == 0 {
+		fmt.Print("Parameters -accesskey, -secretkey and -endpoint should be set\n")
 		flag.Usage()
 		os.Exit(0)
 	}
 
+	// Check arguments
+	args := flag.Args()
+	if len(args) < 2 {
+		flag.Usage()
+		os.Exit(0)
+	}
+	log.Println("copy", args[0], "to", args[1])
+
+	// Connect to teonet S3 storage
+	var connected bool
+	var teoS3conn *teos3.TeoS3
+	connectS3 := func() (con *teos3.TeoS3) {
+		if connected {
+			con = teoS3conn
+			return con
+		}
+		con, err = teos3.Connect(accessKey, secretKey, endpoint, secure)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("connect to s3 storage")
+		connected = true
+		teoS3conn = con
+		return
+	}
+
+	// Check parameters and copy file
+	var source []byte
+	for i := range args {
+
+		// Trim and check S3 in argument
+		args[i] = strings.Trim(args[i], " \t")
+		s3 := strings.Index(args[i], "s3:") == 0
+		var key string
+		if s3 {
+			key = args[i][3:]
+		} else {
+			key = args[i]
+		}
+
+		// Log error function
+		logError := func(err error) {
+			log.Fatalln("error", err)
+		}
+
+		switch i {
+
+		// Source
+		case 0:
+			if s3 {
+				// get S3 object
+				source, err = connectS3().Map.Get(key)
+			} else {
+				// get file
+				source, err = os.ReadFile(key)
+			}
+			if err != nil {
+				logError(err)
+			}
+			log.Println("got data from", args[i])
+
+		// Target
+		case 1:
+			if s3 {
+				// save S3 object
+				err = connectS3().Map.Set(key, source)
+			} else {
+				// save file
+				err = os.WriteFile(key, source, 0644)
+			}
+			if err != nil {
+				logError(err)
+			}
+			log.Println("set data to", args[i])
+		}
+	}
 }
---
+++ tmp/teos3.go
@@ -0,0 +1,172 @@
+// Copyright 2022 Kirill Scherba <kirill@scherba.ru>.  All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// teos3 package contains Golang functions to rasy use AWS S3 storage as
+// KeyValue Database. To use it create next buckets in your S3 storage:
+//
+//	teos3/map
+package teos3
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"sync"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+const Version = "0.0.2"
+
+const teoS3bucket = "teos3"
+
+// TeoS3 methods receiver
+type TeoS3 struct {
+	con *minio.Client
+	*Map
+}
+
+// Connect to teonet S3 storage
+func Connect(accessKey, secretKey, endpoint string, secure bool, buckets ...string) (teos3 *TeoS3, err error) {
+	teos3 = new(TeoS3)
+	teos3.con, err = minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: secure,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var bucket = teoS3bucket
+	if len(buckets) > 0 && len(buckets[0]) > 0 {
+		bucket = buckets[0]
+	}
+	teos3.Map = &Map{teos3.con, bucket}
+	return
+}
+
+// Map methods receiver
+type Map struct {
+	con    *minio.Client
+	bucket string
+}
+
+// Map data struct
+type MapData struct {
+	Key   string `json:"key"`
+	Value []byte `json:"value"`
+}
+
+// Set data to map by key
+func (m *Map) Set(key string, data []byte) (err error) {
+
+	reader := bytes.NewReader(data)
+
+	_, err = m.con.PutObject(context.Background(), m.bucket, key, reader, int64(len(data)),
+		minio.PutObjectOptions{},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// defer obj.Close()
+
+	return
+}
+
+// Get map data by key
+func (m *Map) Get(key string) (data []byte, err error) {
+
+	obj, err := m.con.GetObject(context.Background(), m.bucket, key,
+		minio.GetObjectOptions{},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer obj.Close()
+
+	// Read from raw object
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(obj)
+	data = buf.Bytes()
+
+	return
+}
+
+// Del remove key frim map by key
+func (m *Map) Del(key string) (err error) {
+	err = m.con.RemoveObject(context.Background(), m.bucket, key,
+		minio.RemoveObjectOptions{},
+	)
+
+	return
+}
+
+// Get list of map keys by prefix
+func (m *Map) List(prefix string) (keys chan string, err error) {
+
+	keys = make(chan string, 1)
+
+	go func() {
+		objInfo := m.con.ListObjects(context.Background(), m.bucket,
+			minio.ListObjectsOptions{
+				Prefix: prefix,
+			},
+		)
+		for obj := range objInfo {
+			keys <- obj.Key
+		}
+		close(keys)
+	}()
+
+	return
+}
+
+// Get string array of map keys by prefix
+func (m *Map) ListAr(prefix string) (list []string, err error) {
+
+	objInfo := m.con.ListObjects(context.Background(), m.bucket,
+		minio.ListObjectsOptions{
+			Prefix: prefix,
+		},
+	)
+	for obj := range objInfo {
+		list = append(list, obj.Key)
+	}
+
+	return
+}
+
+// ListBody get all keys values by prefix asynchronously
+func (m *Map) ListBody(prefix string) (mapDatas chan MapData, err error) {
+
+	mapDatas = make(chan MapData, 1)
+
+	objInfo := m.con.ListObjects(context.Background(), m.bucket,
+		minio.ListObjectsOptions{
+			Prefix: prefix,
+		},
+	)
+
+	var wg sync.WaitGroup
+	for obj := range objInfo {
+
+		wg.Add(1)
+		go func(obj minio.ObjectInfo) {
+			data, err := m.Get(obj.Key)
+			if err != nil {
+				return
+			}
+			mapDatas <- MapData{obj.Key, data}
+			wg.Done()
+		}(obj)
+
+	}
+	go func() {
+		wg.Wait()
+		close(mapDatas)
+	}()
+
+	return
+}
